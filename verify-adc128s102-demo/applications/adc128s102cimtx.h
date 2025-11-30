c6a5f930d1685b733dc74409cb92986fb8cab136
Jadelight <67532451+young-nights@users.noreply.github.com>
2025-11-30 15:20:07 +0800

    Update adc128s102cimtx.h

--- verify-adc128s102-demo/applications/adc128s102cimtx.h
+++ verify-adc128s102-demo/applications/adc128s102cimtx.h
@@ -23,7 +23,17 @@
                             HAL_GPIO_WritePin(ADC128S_CS_PORT, ADC128S_CS_PIN, GPIO_PIN_RESET);
 
 
-
+typedef enum
+{
+    ADC128S_Channel_1 = 0,
+    ADC128S_Channel_2,
+    ADC128S_Channel_3,
+    ADC128S_Channel_4,
+    ADC128S_Channel_5,
+    ADC128S_Channel_6,
+    ADC128S_Channel_7,
+    ADC128S_Channel_8
+}adc128s_channel_et;
 
 
 #endif /* APPLICATIONS_BSP_ADC128S102CIMTX_H_ */
